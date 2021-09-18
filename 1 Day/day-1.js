6a85ed6d10a7878dfe2fde3b3809c097c6940163
ABDBXB <abd.albasaleh@hotmail.com>
2021-09-19 01:45:18 +0300

    Edit 1 Day

--- 1 Day/day-1.js
+++ 1 Day/day-1.js
@@ -1,1 +1,28 @@
-console.log("hello world")
+//Primitive Types:
+//.Numbers:
+let a=5;let b=parseFloat("5.44");
+console.log("Nubers A: ",a," B: ",b);
+//.Strings:
+let Name = "Abdulrahman";
+console.log("Hey My Name is: ",Name);
+//.Boolean:
+let GateStatus = true;
+console.log("Gate Status is:" , GateStatus);
+GateStatus = false;
+console.log("Gate Status is:" , GateStatus,"Now");
+//.Null
+let Variable= null;
+console.log(Variable);
+//.Undefined
+let UnVariable= undefined;
+console.log(UnVariable); // Java Script undefined değişkenleri kullanıyor / undefined kullanmak tercih edilmiyor
+
+//Typof
+console.log("----------------");
+console.log(typeof a,typeof b);
+console.log(typeof Name);
+console.log(typeof GateStatus);
+console.log(typeof Variable);
+console.log(typeof UnVariable);
+
+
