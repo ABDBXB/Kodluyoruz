6fba7d83da526f1b42a7cb4f3797e73b8ea74480
ABDBXB <abd.albasaleh@hotmail.com>
2021-09-24 21:56:38 +0300

    add random files

--- 2 Day/3-arrays.js
+++ 2 Day/3-arrays.js
@@ -10,5 +10,12 @@ let array = [1, 2, 3];
 array.forEach((element,index) => {
     console.log(`Element : ${element}, index of it : ${index} `)    
 });
+
+const sum = array.reduce((a,c)=>{
+    return a+c;
+},0);
+console.log(sum)
+
+
 // Reading, writing and deleting elements
 // Array Methods
---
+++ 2 Day/7-promises.js
@@ -0,0 +1,6 @@
+const promise = new Promise(()=>{
+    let x = 0 
+    x=10;
+
+    
+})
