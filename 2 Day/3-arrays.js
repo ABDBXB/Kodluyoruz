5e39de7342de6b1f331fad4e781f6b7fd62d458f
ABDBXB <abd.albasaleh@hotmail.com>
2021-09-24 19:59:43 +0300

    add Homeworks Folder

---
+++ 2 Day/3-arrays.js
@@ -0,0 +1,14 @@
+// Array Literal []
+let array = [1, 2, 3];
+
+
+
+
+
+// Iterating arrays
+
+array.forEach((element,index) => {
+    console.log(`Element : ${element}, index of it : ${index} `)    
+});
+// Reading, writing and deleting elements
+// Array Methods
