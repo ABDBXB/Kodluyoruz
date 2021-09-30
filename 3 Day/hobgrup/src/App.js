e36c3d6ae48ee5084c055424af0f77ab9831fb7c
ABDBXB <abd.albasaleh@hotmail.com>
2021-09-30 19:35:18 +0300

    formating codes

--- 3 Day/hobgrup/src/App.js
+++ 3 Day/hobgrup/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Helloo</h1>
+      </div>
+    );
+  }
 }
 
+
+
+
+
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <Comp1
+//           title={"Hello Coming From Title Prop"}
+//           handleButtonClick={() => {
+//             alert("Button Clicked");
+//           }}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// class Comp1 extends React.Component{
+//   render(){
+//     //Destructuring
+//     const{title,handleButtonClick}=this.props;
+//     return(
+//       <div className="App">
+//         <h1>{title}<br/>Movie App Class Component</h1>
+//         <button onClick={handleButtonClick}>Button Property</button>
+//       </div>
+//     );
+//   }
+// }
+
+// function App(){
+//   return <h1>Hello From Function</h1>
+// }
 export default App;
--- Homeworks/1.Homework/js/script.js
+++ Homeworks/1.Homework/js/script.js
@@ -1,69 +1,74 @@
-
-
 /*************************************************************
-* 					Initializing Variables
-*************************************************************/
+ * 					Initializing Variables
+ *************************************************************/
 
-const API_KEY = 'api_key=389be6178a32f6dac4176b5cde41dada';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
-const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const searchURL = BASE_URL + '/search/movie?'+API_KEY;
-var pageno=0;
-var total_pages=0;
-var searchword="";
+const API_KEY = "api_key=389be6178a32f6dac4176b5cde41dada";
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
+const IMG_URL = "https://image.tmdb.org/t/p/w500";
+const searchURL = BASE_URL + "/search/movie?" + API_KEY;
+var pageno = 0;
+var total_pages = 0;
+var searchword = "";
 //------------------------------------------------
 //Get Search bar with id
-const pagenumber=document.getElementById('pagenumber');
-const searchBar=document.getElementById('searchinput');
-//Add keyup Because We Will Search When User Typing 
-searchBar.addEventListener('keyup' , (e) =>{
-		searchword = e.target.value;
-	if (searchword) {
-		//Get Movies With query To Find it
-		getMovies(searchURL+'&query='+searchword);
-	}else{   //If Search bar is empty Get all Movies
-		getMovies(API_URL);
-	}
-	
+const pagenumber = document.getElementById("pagenumber");
+const searchBar = document.getElementById("searchinput");
+//Add keyup Because We Will Search When User Typing
+searchBar.addEventListener("keyup", (e) => {
+  searchword = e.target.value;
+  if (searchword) {
+    //Get Movies With query To Find it
+    getMovies(searchURL + "&query=" + searchword);
+  } else {
+    //If Search bar is empty Get all Movies
+    getMovies(API_URL);
+  }
 });
 
 //Fetch Data From Url (We Will Give The Function Url To Fethch)
 function getMovies(url) {
   lastUrl = url;
-    fetch(url).then(res => res.json()).then(data => {
-        if(data.results.length !== 0){
-			pageno = data.page;
-			total_pages=data.total_pages;
-			pagenumber.innerText=pageno;
-            showMovies(data.results);
-       }
-    })
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.results.length !== 0) {
+        pageno = data.page;
+        total_pages = data.total_pages;
+        pagenumber.innerText = pageno;
+        showMovies(data.results);
+      }
+    });
 }
 
 //Show All What we get From Fetch With Loop
 function showMovies(data) {
-	const userList = document.getElementById('cardlist');
-	userList.innerHTML = '';
-    data.forEach(movie => {
-        const {title, poster_path, vote_average, overview, id,release_date} = movie;
-        const movieEl = document.createElement('div');
-		//Cut Date With Split To Show it To User In Our Website
-		if (release_date != undefined) {
-			var datearray = release_date.split("-");
-		} else {
-			datearray=['No Date Here'];
-		}
-		if (title.length>11) {
-			var subtitle = title.substring(0,11)+"...";
-		} else{
-			subtitle=title;
-		}
-		//Add Elements In Card Div 
-        movieEl.className="card";
-        movieEl.innerHTML = `
+  const userList = document.getElementById("cardlist");
+  userList.innerHTML = "";
+  data.forEach((movie) => {
+    const { title, poster_path, vote_average, overview, id, release_date } =
+      movie;
+    const movieEl = document.createElement("div");
+    //Cut Date With Split To Show it To User In Our Website
+    if (release_date != undefined) {
+      var datearray = release_date.split("-");
+    } else {
+      datearray = ["No Date Here"];
+    }
+    if (title.length > 11) {
+      var subtitle = title.substring(0, 11) + "...";
+    } else {
+      subtitle = title;
+    }
+    //Add Elements In Card Div
+    movieEl.className = "card";
+    movieEl.innerHTML = `
 		<div class="cardphoto">
-	  <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="">
+	  <img src="${
+      poster_path
+        ? IMG_URL + poster_path
+        : "http://via.placeholder.com/1080x1580"
+    }" alt="">
 	  </div>
 	  <div class="cardname">
 	  <p>${subtitle}</p>
@@ -75,96 +80,90 @@ function showMovies(data) {
 	  <p>${datearray[0]}</p>
 	  </div>
         
-        `
-		//Append it in Cardlist Div
-		userList.appendChild(movieEl);
-		
-    })
+        `;
+    //Append it in Cardlist Div
+    userList.appendChild(movieEl);
+  });
 }
 //------------------------------------------------
-
 
 //==================
 //===  Buttons   ===
 //==================
 //Next Button Function
-function next(){
-	if (!searchword) {					//If User is not Search for Movie
-		if (pageno<total_pages) {
-		let nextpage=pageno+1;
-		getMovies(API_URL+'&page='+nextpage);
-		}else{
-		alert("This is Last Page");
-		getMovies(API_URL+'&page='+total_pages);
-		}
-	}else{			//If User Search for Movie
-		if (pageno<total_pages) {   
-		let nextpage=pageno+1;
-		getMovies(searchURL+'&query='+searchword+'&page='+nextpage);
-		}else{
-		alert("This is Last Page");
-		}
-	}
+function next() {
+  if (!searchword) {
+    //If User is not Search for Movie
+    if (pageno < total_pages) {
+      let nextpage = pageno + 1;
+      getMovies(API_URL + "&page=" + nextpage);
+    } else {
+      alert("This is Last Page");
+      getMovies(API_URL + "&page=" + total_pages);
+    }
+  } else {
+    //If User Search for Movie
+    if (pageno < total_pages) {
+      let nextpage = pageno + 1;
+      getMovies(searchURL + "&query=" + searchword + "&page=" + nextpage);
+    } else {
+      alert("This is Last Page");
+    }
+  }
 }
 //Prev Button Function
-function prev(){
-	if (!searchword) {
-		if (pageno>1) {
-		let prepage=pageno-1;
-		getMovies(API_URL+'&page='+prepage);
-		}else{
-		alert("This is First Page");
-		}
-	}else{
-		if (pageno>1) {
-		let prepage=pageno-1;
-		getMovies(searchURL+'&query='+searchword+'&page='+prepage);
-		}else{
-		alert("This is First Page");
-		}
-	}
+function prev() {
+  if (!searchword) {
+    if (pageno > 1) {
+      let prepage = pageno - 1;
+      getMovies(API_URL + "&page=" + prepage);
+    } else {
+      alert("This is First Page");
+    }
+  } else {
+    if (pageno > 1) {
+      let prepage = pageno - 1;
+      getMovies(searchURL + "&query=" + searchword + "&page=" + prepage);
+    } else {
+      alert("This is First Page");
+    }
+  }
 }
 //last Button Function
-function last(){
-	if (!searchword) {
-	getMovies(API_URL+'&page='+total_pages);
-	}else{
-	getMovies(searchURL+'&query='+searchword+'&page='+total_pages);
-	
-	}
+function last() {
+  if (!searchword) {
+    getMovies(API_URL + "&page=" + total_pages);
+  } else {
+    getMovies(searchURL + "&query=" + searchword + "&page=" + total_pages);
+  }
 }
 //first Button Function
-function first(){
-	if (!searchword) {
-	getMovies(API_URL+'&page=1');
-	}else{
-	getMovies(searchURL+'&query='+searchword+'&page=1');
-	
-	}
+function first() {
+  if (!searchword) {
+    getMovies(API_URL + "&page=1");
+  } else {
+    getMovies(searchURL + "&query=" + searchword + "&page=1");
+  }
 }
 //------------------------------------------------
 
 // Get Elements id Bind With Function On Cilick
-const nebutton= document.getElementById('next');
-	nebutton.addEventListener('click',next);
+const nebutton = document.getElementById("next");
+nebutton.addEventListener("click", next);
 
-const prbutton= document.getElementById('prev');
-	prbutton.addEventListener('click',prev);
+const prbutton = document.getElementById("prev");
+prbutton.addEventListener("click", prev);
 
-const lastbutton= document.getElementById('last');
-	lastbutton.addEventListener('click',last);
+const lastbutton = document.getElementById("last");
+lastbutton.addEventListener("click", last);
 
-const firstbutton= document.getElementById('first');
-	firstbutton.addEventListener('click',first);
+const firstbutton = document.getElementById("first");
+firstbutton.addEventListener("click", first);
 //------------------------------------------------
-	//getMovies(API_URL+'&sort_by=vote_average.asc&page=50');
-	//getMovies(API_URL+'&page=9');
+//getMovies(API_URL+'&sort_by=vote_average.asc&page=50');
+//getMovies(API_URL+'&page=9');
 //Run GitMobvies Function
 getMovies(API_URL);
-
-	
-	
-	
 
 /*
 
