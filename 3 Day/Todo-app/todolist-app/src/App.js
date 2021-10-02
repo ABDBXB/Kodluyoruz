613a9d4570c815653cbc39a3d002f1b6bd0b51d2
ABDBXB <abd.albasaleh@hotmail.com>
2021-10-02 23:47:21 +0300

    Add Context

--- 3 Day/Todo-app/todolist-app/src/App.js
+++ 3 Day/Todo-app/todolist-app/src/App.js
@@ -1,19 +1,28 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import AddNewTodo from "./components/AddNewTodo";
 import TodoList from "./components/TodoList/TodoList";
 import TodoContext from "./context/Todo";
 function App() {
+  const [todoState, setTodoState] = useState({
+    list: [],
+  });
+  const addNewTodo = (text) => {
+    setTodoState({
+      list: [...todoState.list, {text}],
+    });
+  };
   return (
-  <div>
-    <TodoContext.Provider value={{
-      list : [],
-      addNewTodo : (text)=>{},
-
-    }}>
-    <AddNewTodo/>
-    <TodoList/>
-    </TodoContext.Provider>
-  </div>
+    <div>
+      <TodoContext.Provider
+        value={{
+          list: TodoList.list,
+          addNewTodo: addNewTodo ,
+        }}
+      >
+        <AddNewTodo />
+        <TodoList />
+      </TodoContext.Provider>
+    </div>
   );
 }
 
