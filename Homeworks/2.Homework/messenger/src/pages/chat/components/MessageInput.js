784efbcc87bf5ee4c1b38a84fec2aca858200521
ABDBXB <abd.albasaleh@hotmail.com>
2021-10-06 01:48:02 +0300

    Styling input

--- Homeworks/2.Homework/messenger/src/pages/chat/components/MessageInput.js
+++ Homeworks/2.Homework/messenger/src/pages/chat/components/MessageInput.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function MessageInput() {
-  return <div>MessageInput</div>;
+  return (<div className="MessageInput-container">
+    <div className="MessageInput" ><input type="text" placeholder="Type..."/></div>
+    <div className="Messagesendbtn" ><button>Send</button></div>
+    
+  </div>);
 }
