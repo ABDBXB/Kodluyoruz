51c2270378b3cf0d495bf5a60fff8ff22478e450
ABDBXB <abd.albasaleh@hotmail.com>
2021-10-06 00:13:45 +0300

    Header Styling

--- Homeworks/2.Homework/messenger/src/pages/chat/components/UserHeader.js
+++ Homeworks/2.Homework/messenger/src/pages/chat/components/UserHeader.js
@@ -9,7 +9,7 @@ export default function UserHeader() {
   return (<div className="userheader">
     <div className="user-header__userimage"><img src="https://via.placeholder.com/150" alt="noPhoto" /></div>
     <div className="user-header__username"><p>{`${selectedUser.first_name} ${selectedUser.last_name}`}</p></div>
-    <div className="user-header__usermorebtn"><button>More</button></div>
+    <div className="user-header__usermorebtn"><button>?</button></div>
     
     </div>);
 }
