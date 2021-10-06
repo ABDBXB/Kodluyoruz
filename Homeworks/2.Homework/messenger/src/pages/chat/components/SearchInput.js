f337e5be93b06d2e5ed3495dfedc04cabc2e4c1c
ABDBXB <abd.albasaleh@hotmail.com>
2021-10-06 03:08:09 +0300

    Styling Chat

--- Homeworks/2.Homework/messenger/src/pages/chat/components/SearchInput.js
+++ Homeworks/2.Homework/messenger/src/pages/chat/components/SearchInput.js
@@ -1,5 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
+import UserContext from "../../../context/userContext";
 
 export default function SearchInput() {
-  return <div className="Searchbar"><input type="text" placeholder="Search"/></div>;
+  const {search , setSearch} = useState("");
+  const { selectedUser } = React.useContext(UserContext);
+
+
+    
+    
+
+
+  return <div className="Searchbar"><input type="text" placeholder="Search" onChange={(e)=>{setSearch(e.target.value)}}/></div>;
 }
