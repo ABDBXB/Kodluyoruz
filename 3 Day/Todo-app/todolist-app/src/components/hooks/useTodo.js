9251edb56363cb1592e656d265a4a716199847f4
Abdulrahman-Basaleh <abd.albasaleh@hotmail.com>
2021-11-28 15:55:42 +0300

    delete lessons

--- 3 Day/Todo-app/todolist-app/src/components/hooks/useTodo.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import TodoContext  from "../../context/Todo";
-function useTodo() {
-  const { list, removeTodo, addNewTodo } = useContext(TodoContext);
-
-  return {list, removeTodo, addNewTodo };
-}
-
-export default useTodo;
